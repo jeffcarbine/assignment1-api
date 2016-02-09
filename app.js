1edeb4a16ea9afb0f8c2beaf6bd1d625dfa8ab29
jeffcarbine <jeffcarbine@gmail.com>
2016-02-09 09:42:37 -0700

    Cleaner script
    Still works, scripts are cleaner.

--- app.js
+++ app.js
@@ -2,39 +2,19 @@ var reps = require('./lib/reps');
 var express = require('express');
 var app = express();
 
-app.get('/all/by-zip/:zip', function(req, res, next) {
-  reps.allByZip(req.params.zip, function(err, people) {
-    if (err) { return next(err); }
-    res.json(people);
+function makeRoute(route, method, param) {
+  app.get(route, function(req, res, next) {
+    method(req.params[param], function(err, people) {
+      if (err) { return next(err); }
+      res.json(people);
+    });
   });
-});
+}
 
-app.get('/reps/by-name/:names', function(req, res, next) {
-  reps.repsByName(req.params.names, function(err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
-});
-
-app.get('/reps/by-state/:state', function(req, res, next) {
-  reps.repsByState(req.params.state, function(err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
-});
-
-app.get('/sens/by-name/:names', function(req, res, next) {
-  reps.sensByName(req.params.names, function(err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
-});
-
-app.get('/sens/by-state/:state', function(req, res, next) {
-  reps.sensByState(req.params.state, function(err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
-});
+makeRoute('/all/by-zip/:zip', reps.allByZip, 'zip');
+makeRoute('/reps/by-name/:name', reps.repsByName, 'name');
+makeRoute('/reps/by-state/:state', reps.repsByState, 'state');
+makeRoute('/sens/by-name/:name', reps.sensByName, 'name');
+makeRoute('/sens/by-state/:state', reps.sensByState, 'state');
 
 module.exports = app;
