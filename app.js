c564bc98ae451aacc4359777a1fc0dd85fa9e8ac
jeffcarbine <jeffcarbine@gmail.com>
2016-02-09 09:18:17 -0700

    API works now.
    The API passes all tests.

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-var reps = require('./lib/reps.js');
+var reps = require('./lib/reps');
+var express = require('express');
+var app = express();
+
+app.get('/all/by-zip/:zip', function(req, res, next) {
+  reps.allByZip(req.params.zip, function(err, people) {
+    if (err) { return next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/reps/by-name/:names', function(req, res, next) {
+  reps.repsByName(req.params.names, function(err, people) {
+    if (err) { return next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/reps/by-state/:state', function(req, res, next) {
+  reps.repsByState(req.params.state, function(err, people) {
+    if (err) { return next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-name/:names', function(req, res, next) {
+  reps.sensByName(req.params.names, function(err, people) {
+    if (err) { return next(err); }
+    res.json(people);
+  });
+});
+
+app.get('/sens/by-state/:state', function(req, res, next) {
+  reps.sensByState(req.params.state, function(err, people) {
+    if (err) { return next(err); }
+    res.json(people);
+  });
+});
+
+module.exports = app;
