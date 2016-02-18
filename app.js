f24de56bc428fcd443f410f37d65b80a98a83cd7
jeffcarbine <jeffcarbine@gmail.com>
2016-02-18 08:35:18 -0700

    Committing for class.

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-var reps = require('./lib/reps');
 var express = require('express');
+var path = require('path');
+
+// Routes
+var reps = require('./routes/reps');
+
+// Misc
 var app = express();
+var PORT = 8000;
 
-function makeRoute(route, method, param) {
-  app.get(route, function(req, res, next) {
-    method(req.params[param], function(err, people) {
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res, next) {
+  var method;
+
+  if (req.query.type === 'zip') {
+    console.log('Type is zip');
+    method = reps.allByZip;
+  }
+
+  if (method) {
+    method(req.query.search, function(err, people) {
       if (err) { return next(err); }
-      res.json(people);
+      res.render('index', {
+        reps: people,
+        query: req.query,
+      });
     });
-  });
-}
+  } else {
+    res.render('index', {
+      reps: null,
+      query: req.query,
+    });
+  }
 
-makeRoute('/all/by-zip/:zip', reps.allByZip, 'zip');
-makeRoute('/reps/by-name/:name', reps.repsByName, 'name');
-makeRoute('/reps/by-state/:state', reps.repsByState, 'state');
-makeRoute('/sens/by-name/:name', reps.sensByName, 'name');
-makeRoute('/sens/by-state/:state', reps.sensByState, 'state');
+});
+
+app.listen(PORT, function() {
+  console.log('Server started on port ' + PORT);
+});
 
 module.exports = app;
